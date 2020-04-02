9c705cb415c3bca672701f20154311340afc3310
Devin Pastoor <devinp@metrumrg.com>
2020-04-01 21:44:13 -0400

    add: implement use_bbi

--- R/use-bbi.R
+++ R/use-bbi.R
@@ -1,18 +1,16 @@
-#' @title Installing pkgr
+#' @title Installing bbi
 #' @description Identifies system running and return installation instructions
 #' @return character
-#' @examples
-#' use_pkgr()
-#' @rdname use_pkgr
+#' @rdname use_bbi
 #' @export
 #' @importFrom glue glue glue_collapse
 #' @importFrom cli rule
-use_pkgr <- function(){
+use_bbi <- function(){
 
   os <- c('linux','darwin','mingw')
 
   header <- glue::glue(
-    'Installing pkgr on a {cli::col_red(R.version$os)} system',
+    'Installing bbi on a {cli::col_red(R.version$os)} system',
     cli::rule(),.sep = '\n')
 
   footer <- ' '
@@ -22,17 +20,18 @@ use_pkgr <- function(){
   body <-  switch(this_os,
                   'darwin'  = {
                     c('brew tap metrumresearchgroup/homebrew-tap',
-                      'brew install metrumresearchgroup/tap/pkgr')
+                      'brew install bbi')
 
                   },
                   'linux' = {
-                    c(glue::glue('sudo wget {current_release(os = "linux")} -O /tmp/pkgr.tar.gz'),
-                      'sudo tar xzf /tmp/pkgr.tar.gz pkgr',
-                      'sudo mv pkgr /usr/local/bin/pkgr',
-                      'sudo chmod +x /usr/local/bin/pkgr')
+                    c(glue::glue('wget {current_release(os = "linux")} -O /tmp/bbi.tar.gz'),
+                      'tar xzf /tmp/bbi.tar.gz',
+                      'mkdir -p /data/apps',
+                      'mv /tmp/bbi_linux_amd64/bbi /data/apps/bbi',
+                      'chmod +x /usr/local/bin/bbi')
                   },
                   {
-                    c('browse to: https://github.com/metrumresearchgroup/pkgr#getting-started')
+                    c('browse to: https://github.com/metrumresearchgroup/babylon#getting-started')
                   })
 
   glue_this <- c(header,body,footer)
@@ -43,48 +42,37 @@ use_pkgr <- function(){
 
 }
 
-#' @importFrom curl curl_download
 #' @importFrom jsonlite read_json
-current_release <- function(owner = 'metrumresearchgroup', repo = 'pkgr', os = c('linux','darwin','windows')){
+current_release <- function(owner = 'metrumresearchgroup', repo = 'babylon', os = c('linux','darwin','windows')){
 
   tf <- tempfile(fileext = '.json')
 
   on.exit(unlink(tf),add = TRUE)
 
-  release_json <- curl::curl_download(glue::glue('https://api.github.com/repos/{owner}/{repo}/releases/latest'),destfile = tf,quiet = TRUE)
+  release_info <- jsonlite::fromJSON(glue::glue('https://api.github.com/repos/{owner}/{repo}/releases/latest'), simplifyDataFrame = FALSE)
 
-  release_info <- jsonlite::read_json(release_json)
 
   uris <- grep('gz$',sapply(release_info$assets,function(x) x$browser_download_url),value = TRUE)
-
-  names(uris) <- sapply(strsplit(gsub('_amd64.tar.gz$','',basename(uris)),'_'),'[[',3)
+  uris <- uris[grepl(x = uris, pattern = "amd64", fixed = TRUE)]
+  names(uris) <- sapply(strsplit(gsub('_amd64.tar.gz$','',basename(uris)),'_'),'[[',2)
 
   uris[os]
 
 }
 
 
-#' @title FUNCTION_TITLE
-#' @description FUNCTION_DESCRIPTION
-#' @param os PARAM_DESCRIPTION, Default: 'linux'
-#' @return OUTPUT_DESCRIPTION
-#' @details DETAILS
-#' @examples
-#' \dontrun{
-#' if(interactive()){
-#'  #EXAMPLE1
-#'  }
-#' }
-#' @rdname pkgr.current_release
+#' @title current release
+#' @param os operating system, Default: 'linux'
+#' @rdname bbi_current_release
 #' @export
-pkgr.current_release <- function(os = "linux"){
+bbi_current_release <- function(os = "linux"){
   gsub('^v','',basename(dirname(current_release(os = os))))
 }
 
 install_menu <- function(body,this_os){
 
-  release_v <- pkgr.current_release()
-  local_v <- pkgr.version()
+  release_v <- bbi_current_release()
+  local_v <- bbi_version()
 
   if(!identical(release_v,local_v)){
 
@@ -94,23 +82,37 @@ install_menu <- function(body,this_os){
 
       if(utils::menu(choices = c('Yes','No'))==1){
         system(paste(body,collapse =' ; '))
-        local_v <- pkgr.version()
+        local_v <- bbi_version()
       }
 
     }
 
   }
 
-  print(version_message(local_v = local_v,release_v = release_v))
+  version_message(local_v = local_v,release_v = release_v)
 
+}
+
+#' @title bbi version
+#' @description Returns string of installed bbi cli version
+#' @return character
+#' @examples
+#' \dontrun{
+#' bbi_version()
+#' }
+#' @rdname bbi_version
+#' @export
+bbi_version <- function(){
+  bbi_path <- getOption('rbabylon.bbi_exe_path', Sys.which('bbi'))
+  system(sprintf('%s version', bbi_path),intern = TRUE)
 }
 
 #' @importFrom cli rule col_blue
 #' @importFrom glue glue
-version_message <- function(local_v = pkgr.version(), release_v = pkgr.current_release()){
+version_message <- function(local_v = bbi_version(), release_v = bbi_current_release()){
 
   flag <- ifelse(identical(release_v,local_v),"Current Release","Not Current Release")
 
-  glue::glue(cli::rule(left = cli::col_blue('pkgr Version: {local_v} ({flag}) is Installed'),line = 2))
-
+  cat(glue::glue(cli::col_blue('- Current release: {release_v}\n')))
+  cat(glue::glue(cli::col_blue('- Installed Version: {local_v}')))
 }
