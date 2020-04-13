7902d7dc79ad9a1332477058e72f1aa8de23d4be
seth127 <seth127@gmail.com>
2020-04-13 11:39:57 -0400

    take compososable-bbi off of drone because no NONMEM

--- tests/testthat/test-workflow-composable-bbi.R
+++ tests/testthat/test-workflow-composable-bbi.R
@@ -5,8 +5,9 @@
 # Because of this, it is disabled unless on Metworx.
 ####################################################
 
-if (Sys.getenv("METWORX_VERSION") == "" && Sys.getenv("DRONE") != "true") {
-  skip("test-summary only runs on Metworx or Drone")
+# can't run on Drone because there's no NONMEM
+if (Sys.getenv("METWORX_VERSION") == "") {
+  skip("test-summary only runs on Metworx")
 }
 
 # define constants
