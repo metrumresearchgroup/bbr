c636a89cde4b2e5ec7e871af4e0f43dc523c2139
seth127 <seth127@gmail.com>
2020-04-23 09:15:41 -0400

    WIP: added filter and bind for 101 model

--- tests/testthat/test-parse_labels.R
+++ tests/testthat/test-parse_labels.R
@@ -15,18 +15,19 @@ options('rbabylon.bbi_exe_path' = '/data/apps/bbi')
 set_model_directory(MODEL_DIR)
 bbi_init(MODEL_DIR, "/opt/NONMEM", "nm74gf")
 
-MODEL_PICK <- "510" ### 101 triggers fails because of this error https://github.com/metrumresearchgroup/babylon/issues/163
+MODEL_PICK <- "101" ### 101 triggers fails because of this error https://github.com/metrumresearchgroup/babylon/issues/163
 
 ref_df <- readRDS(glue("data/{MODEL_PICK}_PARAMTBL.rds"))[[1]]
 
 if (fs::file_exists(file.path(MODEL_DIR, glue("{MODEL_PICK}.yaml")))) fs::file_delete(file.path(MODEL_DIR, glue("{MODEL_PICK}.yaml")))
 .mod <-  rbabylon::new_model(glue("{MODEL_PICK}.yaml"), glue("the {MODEL_PICK} model"))
 .param_df <- .mod %>% rbabylon::model_summary() %>% param_estimates()
+.param_df
 
 #.ctl_raw <- readr::read_file(glue("../../inst/extdata/{MODEL_PICK}.ctl"))
-.ctl_path <- glue("../../inst/extdata/{MODEL_PICK}.ctl")
+#.ctl_path <- glue("../../inst/extdata/{MODEL_PICK}.ctl")
 
-
+# load from model object
 .label_df <- .mod %>% param_labels()
 .label_df
 
@@ -34,12 +35,16 @@ if (fs::file_exists(file.path(MODEL_DIR, glue("{MODEL_PICK}.yaml")))) fs::file_d
 
 # # join
 # .new_df <- left_join(.param_df, .label_df)
-.new_df <- bind_cols(.param_df, .label_df %>% select(-names)) # bind instead, but you have to filter .param_df for diagonals
-names(.new_df) <- toupper(names(.new_df))
-.new_df
+.new_df <- bind_cols(.param_df, .label_df %>% select(-names))
 
+# for 101 filter out off-diagonal sigma
+.new_df <- bind_cols(
+  .param_df %>% filter(!(str_detect(names, "SIGMA") & diag == FALSE)),
+  .label_df %>% select(-names)
+) # bind instead, but you have to filter .param_df for diagonals
 
 # join against reference to see if they're the same
+names(.new_df) <- toupper(names(.new_df))
 ref_df %>% full_join(.new_df, by = c("LABEL", "UNIT", "TYPE")) %>% select(PARAM, NAMES, LABEL, UNIT, TYPE, value, ESTIMATE, se, STDERR)
 
 
