65f25884ea0b3e4c2a9c496907dcace678e91d0b
seth127 <seth127@gmail.com>
2020-06-22 09:31:22 -0400

    skip_if_over_rate_limit docs addition

--- tests/testthat/helpers.R
+++ tests/testthat/helpers.R
@@ -1,4 +1,6 @@
 skip_if_over_rate_limit <- function(by = 5) {
+  # this uses no token, so it is checking the public rate limit.
+  # this is appropriate because this package has no interface for passing in a token, so it will always be hitting the public API.
 
   res <- jsonlite::fromJSON("https://api.github.com/rate_limit", simplifyDataFrame = FALSE)
 
