677c9d71a781b29167b9e00de86bd1d8d150874b
copernican <copernican@gmail.com>
2020-10-02 13:16:54 -0400

    test: consolidate tests of _log() function behavior when YAML is malformed

--- tests/testthat/test-run-log.R
+++ tests/testthat/test-run-log.R
@@ -13,8 +13,22 @@ withr::with_options(list(rbabylon.model_directory = NULL), {
     log_df <- expect_error(run_log(), regexp = "`.base_dir` cannot be `NULL`")
   })
 
-  test_that("run_log errors with malformed YAML", {
-    log_df <- expect_error(run_log(getwd()), regexp = "Unexpected error trying to read yaml")
+  test_that("run_log() errors with malformed YAML", {
+    temp_dir <- tempdir()
+    temp_yaml <- fs::file_copy("test-yaml/zz_fail_no_modtype.yaml", temp_dir)
+    on.exit({
+      fs::file_delete(temp_yaml)
+      fs::file_delete(file.path(temp_dir, basename(YAML_TEST_FILE)))
+    })
+
+    expect_warning(log_df <- run_log(temp_dir), "do not contain required keys")
+    expect_true(nrow(log_df) == 0L)
+
+    fs::file_copy(YAML_TEST_FILE, temp_dir)
+    expect_error(
+      run_log(temp_dir),
+      regexp = "Unexpected error trying to read yaml"
+    )
   })
 
   test_that("run_log returns NULL and warns when no YAML found", {
