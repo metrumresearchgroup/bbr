de721e6e73aface09a5fa7ba4d4493a34daba8fb
copernican <copernican@gmail.com>
2020-10-07 21:24:08 -0400

    test: add integration test that `.config_path` argument works as intended

--- tests/testthat/test-submit-model.R
+++ tests/testthat/test-submit-model.R
@@ -79,4 +79,49 @@ withr::with_options(list(rbabylon.model_directory = NULL), {
     )
   })
 
+  test_that("submit_model() works with non-NULL .config_path", {
+    skip_if(Sys.getenv("SKIP_BBI_TEST") == "true")
+
+    test_dir <- getwd()
+    withr::with_tempdir({
+      # temporarily put babylon.yaml in the temp dir
+      withr::with_options(list(rbabylon.bbi_exe_path = read_bbi_path()), {
+        bbi_init(".", "/opt/NONMEM", "nm74gf")
+      })
+
+      # copy model, YAML, and data files to the same location
+      files_to_copy <- file.path(
+        test_dir,
+        MODEL_DIR,
+        c("1.ctl", "1.yaml", "../data/acop.csv")
+      )
+
+      purrr::walk(files_to_copy, fs::file_copy, ".")
+
+      # modify DATA to reflect location in temp dir
+      readr::read_file("1.ctl") %>%
+        stringr::str_replace("\\$DATA\\s+[^\\s]+", "$DATA ../acop.csv") %>%
+        readr::write_file("1.ctl")
+
+      # set up temp config in a different location
+      temp_config <- tempfile(fileext = ".yaml")
+      fs::file_copy("babylon.yaml", temp_config)
+      on.exit(fs::file_delete(temp_config))
+
+      # and delete babylon.yaml from the temp dir
+      fs::file_delete("babylon.yaml")
+
+      mod <- read_model("1")
+      res <- submit_model(
+        mod,
+        .mode = "local",
+        .bbi_exe_path = read_bbi_path(),
+        .config_path = temp_config,
+        .wait = TRUE
+      )
+
+      expect_true(any(grepl("models completed", res[["stdout"]], fixed = TRUE)))
+    })
+  })
+
 }) # closing withr::with_options
