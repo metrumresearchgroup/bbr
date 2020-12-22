2a7e2358f9f73a1eb2ed7d1e1fba39a70a833439
seth127 <seth127@gmail.com>
2020-12-22 11:46:36 -0500

    test: Added data-raw/build-print-refs.R and rendered references in test-refs/print-refs/

--- data-raw/build-print-refs.R
+++ data-raw/build-print-refs.R
@@ -9,18 +9,20 @@ devtools::load_all() # can't call library(rbabylon) from within rbabylon when us
 #'
 #' Saves to `inst/test-refs/print-refs/`.
 #' @param .s bbi_nonmem_summary object
-build_print_bbi_nonmem_summary_refs <- function(.s) {
-  root_dir <- file.path(get_output_dir(.s), get_model_id(.s))
+#' @param .args list of arguments to pass through to print method
+#' @param .suffix optional suffix to put on output file name
+build_print_bbi_nonmem_summary_refs <- function(.s, .args = list(), .suffix = "") {
   out_dir <- system.file("test-refs", "print-refs", package = "rbabylon")
-  message(glue::glue("Writing test-print.R refs for {root_dir} into {out_dir}"))
 
-  #
-  out_stem <- file.path(out_dir, paste0("print_bbi_nonmem_summary_", get_model_id(.s)))
+  # build output path
+  .suffix <- ifelse(.suffix == "", .suffix, paste0("_", .suffix))
+  out_path <- file.path(out_dir, paste0("print_bbi_nonmem_summary_", get_model_id(.s), .suffix, ".txt"))
+  message(glue::glue("Writing test-print.R refs for {get_model_id(.s)} into {out_path}"))
 
-  # NEED TO ADD ITERATING OVER ARGS (pass args in as a list of lists? then use do.call?)
-  print_str <- capture.output(print(.s))
-  writeLines(print_str, out_stem)
-
+  # capture print output and write to file
+  .args[["x"]] <- .s
+  print_str <- capture.output(do.call("print", .args))
+  writeLines(print_str, out_path)
 }
 
 ###################
@@ -34,4 +36,33 @@ file.path(.proj_root, "inst", "model", "nonmem", "basic", 1) %>%
   read_model() %>%
   model_summary() %>%
   build_print_bbi_nonmem_summary_refs()
+
+file.path(.proj_root, "inst", "model", "nonmem", "complex", "iovmm") %>%
+  read_model() %>%
+  model_summary() %>%
+  build_print_bbi_nonmem_summary_refs()
+
+file.path(.proj_root, "inst", "model", "nonmem", "complex", "1001") %>%
+  read_model() %>%
+  model_summary(.bbi_args = list(ext_file = "1001.1.TXT")) %>%
+  build_print_bbi_nonmem_summary_refs()
+
+iov_sum <- file.path(.proj_root, "inst", "model", "nonmem", "complex", "acop-iov") %>%
+  read_model() %>%
+  model_summary()
+
+build_print_bbi_nonmem_summary_refs(iov_sum, .suffix = "noargs")
+build_print_bbi_nonmem_summary_refs(iov_sum, .args = list(.fixed = TRUE), .suffix = "fixedTRUE")
+build_print_bbi_nonmem_summary_refs(iov_sum, .args = list(.fixed = TRUE, .nrow = 15), .suffix = "fixedTRUE_nrow15")
+
+# write out args scenarios
+saem_imp_sum <- file.path(.proj_root, "inst", "model", "nonmem", "complex", "example2_saemimp") %>%
+  read_model() %>%
+  model_summary()
+
+build_print_bbi_nonmem_summary_refs(saem_imp_sum, .suffix = "noargs")
+build_print_bbi_nonmem_summary_refs(saem_imp_sum, .args = list(.fixed = TRUE), .suffix = "fixedTRUE")
+build_print_bbi_nonmem_summary_refs(saem_imp_sum, .args = list(.off_diag = TRUE), .suffix = "offdiagTRUE")
+build_print_bbi_nonmem_summary_refs(saem_imp_sum, .args = list(.fixed = TRUE, .off_diag = TRUE), .suffix = "fixedTRUE_offdiagTRUE")
+
 
