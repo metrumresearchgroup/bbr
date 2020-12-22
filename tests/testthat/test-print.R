98a314e5fb333cbee64e63afa5d01fdb9481d874
seth127 <seth127@gmail.com>
2020-12-22 11:46:57 -0500

    test: added tests for print.bbi_nonmem_summary

--- tests/testthat/test-print.R
+++ tests/testthat/test-print.R
@@ -4,6 +4,7 @@ skip_if_not_drone_or_metworx("test-print")
 
 model_dir <- ABS_MODEL_DIR
 mod_ctl_path <- file.path(model_dir, CTL_FILENAME)
+PRINT_REF_DIR <- file.path(REF_DIR, "print-refs")
 
 withr::with_options(list(rbabylon.bbi_exe_path = read_bbi_path()), {
 
@@ -44,6 +45,120 @@ withr::with_options(list(rbabylon.bbi_exe_path = read_bbi_path()), {
     expect_true(str_detect(call_str, read_bbi_path()))
     expect_false(str_detect(call_str, temp_dir))
     expect_true(str_detect(call_str, "--overwrite --threads=4"))
+  })
+
+  test_that("print.bbi_nonmem_summary works basic FOCE model", {
+    res_str <- system.file("model", "nonmem", "basic", "1", package = "rbabylon") %>%
+      read_model() %>%
+      model_summary() %>%
+      print() %>%
+      capture.output()
+
+    ref_str <- readLines(file.path(PRINT_REF_DIR, "print_bbi_nonmem_summary_1.txt"))
+    expect_equal(res_str, ref_str)
+  })
+
+  test_that("print.bbi_nonmem_summary works mixture model", {
+    res_str <- expect_warning({
+      system.file("model", "nonmem", "complex", "iovmm", package = "rbabylon") %>%
+        read_model() %>%
+        model_summary() %>%
+        print() %>%
+        capture.output()
+      }, regexp = "param_estimates.+mixture model"
+    )
+
+    ref_str <- readLines(file.path(PRINT_REF_DIR, "print_bbi_nonmem_summary_iovmm.txt"))
+    expect_equal(res_str, ref_str)
+  })
+
+  test_that("print.bbi_nonmem_summary works Bayes model", {
+    res_str <- system.file("model", "nonmem", "complex", "1001", package = "rbabylon") %>%
+      read_model() %>%
+      model_summary(.bbi_args = list(ext_file = "1001.1.TXT")) %>%
+      print() %>%
+      capture.output()
+
+    ref_str <- readLines(file.path(PRINT_REF_DIR, "print_bbi_nonmem_summary_1001.txt"))
+    expect_equal(res_str, ref_str)
+  })
+
+  test_that("print.bbi_nonmem_summary works SAEM-IMP model", {
+    res_str <- system.file("model", "nonmem", "complex", "example2_saemimp", package = "rbabylon") %>%
+      read_model() %>%
+      model_summary() %>%
+      print() %>%
+      capture.output()
+
+    ref_str <- readLines(file.path(PRINT_REF_DIR, "print_bbi_nonmem_summary_example2_saemimp_noargs.txt"))
+    expect_equal(res_str, ref_str)
+  })
+
+  test_that("print.bbi_nonmem_summary works IOV model", {
+    # load a model summary
+    res_str <- system.file("model", "nonmem", "complex", "acop-iov", package = "rbabylon") %>%
+      read_model() %>%
+      model_summary() %>%
+      print() %>%
+      capture.output()
+
+    ref_str <- readLines(file.path(PRINT_REF_DIR, "print_bbi_nonmem_summary_acop-iov_noargs.txt"))
+    expect_equal(res_str, ref_str)
+  })
+
+  test_that("print.bbi_nonmem_summary .fixed=TRUE", {
+    # check with IOV model
+    res_str <- system.file("model", "nonmem", "complex", "acop-iov", package = "rbabylon") %>%
+      read_model() %>%
+      model_summary() %>%
+      print(.fixed = TRUE) %>%
+      capture.output()
+
+    ref_str <- readLines(file.path(PRINT_REF_DIR, "print_bbi_nonmem_summary_acop-iov_fixedTRUE.txt"))
+    expect_equal(res_str, ref_str)
+
+    # check with SAEM-IMP model
+    res_str <- system.file("model", "nonmem", "complex", "example2_saemimp", package = "rbabylon") %>%
+      read_model() %>%
+      model_summary() %>%
+      print(.fixed = TRUE) %>%
+      capture.output()
+
+    ref_str <- readLines(file.path(PRINT_REF_DIR, "print_bbi_nonmem_summary_example2_saemimp_fixedTRUE.txt"))
+    expect_equal(res_str, ref_str)
+  })
+
+  test_that("print.bbi_nonmem_summary .nrow argument", {
+    # load a model summary
+    res_str <- system.file("model", "nonmem", "complex", "acop-iov", package = "rbabylon") %>%
+      read_model() %>%
+      model_summary() %>%
+      print(.nrow = 15, .fixed = TRUE) %>%
+      capture.output()
+
+    ref_str <- readLines(file.path(PRINT_REF_DIR, "print_bbi_nonmem_summary_acop-iov_fixedTRUE_nrow15.txt"))
+    expect_equal(res_str, ref_str)
+  })
+
+
+  test_that("print.bbi_nonmem_summary .off_diag=TRUE", {
+    .s <- system.file("model", "nonmem", "complex", "example2_saemimp", package = "rbabylon") %>%
+      read_model() %>%
+      model_summary()
+
+    # check without .fixed=TRUE
+    res_str <- .s %>%
+      print(.off_diag = TRUE) %>%
+      capture.output()
+    ref_str <- readLines(file.path(PRINT_REF_DIR, "print_bbi_nonmem_summary_example2_saemimp_offdiagTRUE.txt"))
+    expect_equal(res_str, ref_str)
+
+    # check with .fixed=TRUE
+    res_str <- .s %>%
+      print(.off_diag = TRUE, .fixed = TRUE) %>%
+      capture.output()
+    ref_str <- readLines(file.path(PRINT_REF_DIR, "print_bbi_nonmem_summary_example2_saemimp_fixedTRUE_offdiagTRUE.txt"))
+    expect_equal(res_str, ref_str)
   })
 
 })
