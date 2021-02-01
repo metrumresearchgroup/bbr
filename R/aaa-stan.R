22682da3f0b08c04fb9f71e4b5be9e148d3fd985
seth127 <seth127@gmail.com>
2021-02-01 14:55:01 -0500

    feat: changed standata() function to take .dir arg

--- R/aaa-stan.R
+++ R/aaa-stan.R
@@ -7,15 +7,16 @@ model{}"
 STANMOD_SCAFFOLD_MD5 <- "d7d6e3b78a009985f977e068e09caea1"
 
 
-STANDATA_SCAFFOLD_STRING <- "standata <- function() {
+STANDATA_SCAFFOLD_STRING <- "standata <- function(.dir) {
   # read in any input data
+  in_data <- readr::read_csv(file.path(.dir, '..', 'my_data.csv'))
 
   # do any transformations
 
   # return the list that can be passed to cmdstanr
 }"
 
-STANDATA_SCAFFOLD_MD5 <- "5ae61eeedc4ef564d041f2d18a7e5fb5"
+STANDATA_SCAFFOLD_MD5 <- "45f2b82bf31010ef018da0827c5314a0"
 
 
 STANINIT_SCAFFOLD_STRING <- "init <- function() {
