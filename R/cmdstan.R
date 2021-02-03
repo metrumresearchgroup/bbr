f72eb6f7158f764d24faee5fb21adca2a4b87941
seth127 <seth127@gmail.com>
2021-02-03 11:16:21 -0500

    feat: added  check_model_up_to_date() function

--- R/cmdstan.R
+++ R/cmdstan.R
@@ -283,7 +283,7 @@ import_stan_init <- function(.mod, .standata) {
 
 #' Parse args to be passed to cmdstanr
 #'
-#' Any arguments destined for [cmdstanr::sample()] will be passed in
+#' Any arguments destined for `cmdstanr::sample()` will be passed in
 #' via `...`. This function checks that those are valid and then writes
 #' the resulting list of args to a file for reproducibility checking.
 #'
@@ -292,7 +292,7 @@ import_stan_init <- function(.mod, .standata) {
 #'
 #' @param .mod the `bbi_stan_model` object
 #' @param .valid_stanargs A character vector of valid arguments to pass
-#'   through to [cmdstanr::sample()]
+#'   through to `cmdstanr::sample()`
 #' @param ... The arguments to capture and check
 #'
 #' @return the named list of parsed and checked args
@@ -363,8 +363,8 @@ build_stan_bbi_config <- function(.mod, .write) {
     !!STANCFG_DATA_MD5   := tools::md5sum(build_path_from_model(.mod, STANDATA_R_SUFFIX)),
     !!STANCFG_INIT_PATH  := build_path_from_model(.mod, STANINIT_SUFFIX),
     !!STANCFG_INIT_MD5   := tools::md5sum(build_path_from_model(.mod, STANINIT_SUFFIX)),
-    !!STANCFG_ARGS_PATH  := build_path_from_model(.mod, STANINIT_SUFFIX),
-    !!STANCFG_ARGS_MD5   := tools::md5sum(build_path_from_model(.mod, STANINIT_SUFFIX)),
+    !!STANCFG_ARGS_PATH  := build_path_from_model(.mod, STANARGS_SUFFIX),
+    !!STANCFG_ARGS_MD5   := tools::md5sum(build_path_from_model(.mod, STANARGS_SUFFIX)),
     "configuration" = rlang::list2(
       "cmdstan_version"     = cmdstanr::cmdstan_version(),
       "cmdstanr_version"    = as.character(packageVersion('cmdstanr')),
@@ -379,76 +379,4 @@ build_stan_bbi_config <- function(.mod, .write) {
   return(stan_config)
 }
 
-
-#' Check that a Stan model is up-to-date
-#'
-#' Specifically, check that required files on disk
-#' have not changed since the model was run. This is accomplished
-#' by taking their md5 hashes and comparing it to the hashes
-#' stored in `bbi_config.json`
-#'
-#' @importFrom jsonlite fromJSON
-#' @importFrom fs file_exists
-#'
-#' @param .mod a `bbi_stan_model` object
-#' @param .build_data If `FALSE`, the default, check the hashes of the
-#'   `-standata.R` and `-standata.json` against the hashes in `bbi_config.json`.
-#'   If `TRUE`, run `-standata.R` and save the output to a temp file and check
-#'   the hash of the temp file against the `bbi_config.json` hash. The second
-#'   option actually runs the code and, importantly, as verifies that the input
-#'   data has not changed.
-#'
-#' @return `TRUE` if nothing has changed, `FALSE` if anything has changed.
-#' @keywords internal
-check_stan_output_up_to_date <- function(.mod, .build_data = FALSE) {
-
-  # check model and load config
-  check_stan_model(.mod, error = TRUE)
-  config_path <- file.path(get_output_dir(.mod), "bbi_config.json")
-  if (!fs::file_exists(config_path)) {
-    stop(glue("Cannot check if {get_model_id(.mod)} is up-to-date because it has not been run."))
-  }
-  config <- jsonlite::fromJSON(config_path)
-
-  # check necessary files for changes
-  changed_files <- c()
-
-  changed_files <- c(
-    changed_files,
-    config[[CONFIG_MODEL_MD5]] != tools::md5sum(get_model_path(.mod))
-  )
-
-  changed_files <- c(
-    changed_files,
-    config[[STANCFG_INIT_MD5]] != tools::md5sum(build_path_from_model(.mod, STANINIT_SUFFIX))
-  )
-
-  changed_files <- c(
-    changed_files,
-    config[[STANCFG_ARGS_MD5]] != tools::md5sum(build_path_from_model(.mod, STANARGS_SUFFIX))
-  )
-
-  if (isTRUE(.build_data)) {
-    stop("Not implemented")
-  } else {
-    changed_files <- c(
-      changed_files,
-      config[[STANCFG_DATA_MD5]] != tools::md5sum(build_path_from_model(.mod, STANDATA_R_SUFFIX)),
-      config[[CONFIG_DATA_MD5]] != tools::md5sum(build_path_from_model(.mod, STANDATA_JSON_SUFFIX))
-    )
-  }
-
-  any_changes <- any(changed_files)
-
-  if(isTRUE(any_changes)) {
-    message(paste(
-      glue("The following files have changed in {get_model_id(.mod)}"),
-      paste("*", names(which(changed_files)), collapse = "\n"),
-      sep = "\n"
-    ))
-  }
-
-
-  return(!any_changes)
-}
 
