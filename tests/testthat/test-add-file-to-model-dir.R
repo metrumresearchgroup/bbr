b6fe6f4985d060e23204359e7b0b90dcd1662373
seth127 <seth127@gmail.com>
2021-03-10 12:08:02 -0500

    test: adding tests for check_stan_model() and add_file_ helpers. Also fixed the annoying use_bbi() printing that was sneaking through .quiet=TRUE

---
+++ tests/testthat/test-add-file-to-model-dir.R
@@ -0,0 +1,73 @@
+context("adding files to Stan models")
+
+test_that("add_stan_file() works correctly for scaffold", {
+  mod_name <- "testmod_add_stan_file1"
+  suppressMessages(
+    .m <- new_model(mod_name, .model_type = "stan")
+  )
+  on.exit(cleanup_model(.m))
+
+  # check default
+  fs::file_delete(get_model_path(.m))
+  .m <- add_stan_file(.m)
+  expect_equal(
+    STANMOD_SCAFFOLD_MD5,
+    as.character(tools::md5sum(get_model_path(.m)))
+  )
+
+  # check passing .source_file
+  .sf <- get_model_path(STAN_MOD1)
+  .m <- add_stan_file(.m, .source_file = .sf)
+  expect_equal(
+    as.character(tools::md5sum(.sf)),
+    as.character(tools::md5sum(get_model_path(.m)))
+  )
+})
+
+test_that("add_standata_file() works correctly for scaffold", {
+  mod_name <- "testmod_add_standata_file1"
+  suppressMessages(
+    .m <- new_model(mod_name, .model_type = "stan")
+  )
+  on.exit(cleanup_model(.m))
+
+  # check default
+  fs::file_delete(build_path_from_model(.m, STANDATA_R_SUFFIX))
+  .m <- add_standata_file(.m)
+  expect_equal(
+    STANDATA_SCAFFOLD_MD5,
+    as.character(tools::md5sum(build_path_from_model(.m, STANDATA_R_SUFFIX)))
+  )
+
+  # check passing .source_file
+  .sf <- build_path_from_model(STAN_MOD1, STANDATA_R_SUFFIX)
+  .m <- add_standata_file(.m, .source_file = .sf)
+  expect_equal(
+    as.character(tools::md5sum(.sf)),
+    as.character(tools::md5sum(build_path_from_model(.m, STANDATA_R_SUFFIX)))
+  )
+})
+
+test_that("add_stan_init() works correctly for scaffold", {
+  mod_name <- "testmod_add_stan_init1"
+  suppressMessages(
+    .m <- new_model(mod_name, .model_type = "stan")
+  )
+  on.exit(cleanup_model(.m))
+
+  # check default
+  fs::file_delete(build_path_from_model(.m, STANINIT_SUFFIX))
+  .m <- add_stan_init(.m)
+  expect_equal(
+    STANINIT_SCAFFOLD_MD5,
+    as.character(tools::md5sum(build_path_from_model(.m, STANINIT_SUFFIX)))
+  )
+
+  # check passing .source_file
+  .sf <- build_path_from_model(STAN_MOD1, STANINIT_SUFFIX)
+  .m <- add_stan_init(.m, .source_file = .sf)
+  expect_equal(
+    as.character(tools::md5sum(.sf)),
+    as.character(tools::md5sum(build_path_from_model(.m, STANINIT_SUFFIX)))
+  )
+})
--- tests/testthat/test-new-model.R
+++ tests/testthat/test-new-model.R
@@ -163,21 +163,22 @@ test_that("read_model() works for Stan model", {
 test_that("new_model() errors for Stan model without .model_type", {
   skip_if_no_stan("new_model() errors for Stan model without .model_type")
   expect_error(
-    new_model(file.path(STAN_MODEL_DIR, "testmod")),
+    new_model(file.path(STAN_MODEL_DIR, "testmod_new_model1")),
     regexp = NONMEM_MODEL_TYPE_ERR_MSG
   )
 })
 
 test_that("new_model() works for Stan model", {
   skip_if_no_stan("new_model() works for Stan model")
+  mod_name <- "testmod_new_model2"
   expect_message(
-    .m <- new_model(file.path(STAN_MODEL_DIR, "testmod"), .model_type = "stan"),
+    .m <- new_model(file.path(STAN_MODEL_DIR, mod_name), .model_type = "stan"),
     regexp = MISSING_STAN_FILES_ERR_MSG
   )
   on.exit(cleanup_model(.m))
 
   expect_equal(.m[[YAML_MOD_TYPE]], "stan")
-  expect_equal(.m[[ABS_MOD_PATH]], file.path(STAN_ABS_MODEL_DIR, "testmod"))
+  expect_equal(.m[[ABS_MOD_PATH]], file.path(STAN_ABS_MODEL_DIR, mod_name))
   expect_s3_class(.m, STAN_MOD_CLASS)
   expect_s3_class(.m, BBI_PARENT_CLASS)
 })
