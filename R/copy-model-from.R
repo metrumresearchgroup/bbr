4e2af83415dbc70add4080cf253951a4d52971b5
seth127 <seth127@gmail.com>
2021-03-10 15:42:06 -0500

    feat: added copy_model_from.bbi_stan_model dispatch with a test. In the process, refactored privated implementation function for copy_model_from().

--- R/copy-model-from.R
+++ R/copy-model-from.R
@@ -26,11 +26,11 @@
 #' @param .inherit_tags If `FALSE`, the default, new model will only have any
 #'   tags passed to `.add_tags` argument. If `TRUE` inherit any tags from
 #'   `.parent_mod`, with any tags passed to `.add_tags` appended.
-#' @param .update_model_file If `TRUE`, the default, update the newly created
-#'   model file. If `FALSE`, new model file will be an exact copy of its parent.
-#'   For a NONMEM model, this currently means only the `$PROBLEM` line in the
-#'   new control stream will be updated to read `See {.new_model}.yaml. Created
-#'   by bbr.`.
+#' @param .update_model_file **Only relevant to NONMEM models.** If `TRUE`, the
+#'   default, update the newly created model file. If `FALSE`, new model file
+#'   will be an exact copy of its parent. For a NONMEM model, this currently
+#'   means only the `$PROBLEM` line in the new control stream will be updated to
+#'   read `See {.new_model}.yaml. Created by bbr.`.
 #' @param .overwrite If `FALSE`, the default,  function will error if a model
 #'   file already exists at specified `.new_model` path. If `TRUE` any existing
 #'   file at `.new_model` will be overwritten silently.
@@ -81,7 +81,7 @@ copy_model_from.bbi_nonmem_model <- function(
 
   .new_model <- build_new_model_path(.parent_mod, .new_model)
 
-  .mod <- copy_nonmem_model_from(
+  .mod <- copy_model_from_impl(
     .parent_mod = .parent_mod,
     .new_model = .new_model,
     .description = .description,
@@ -89,33 +89,16 @@ copy_model_from.bbi_nonmem_model <- function(
     .add_tags = .add_tags,
     .inherit_tags = .inherit_tags,
     .update_model_file = .update_model_file,
-    .overwrite = .overwrite
+    .overwrite = .overwrite,
+    .model_type = "nonmem"
   )
 
   return(.mod)
 }
 
-#####################################
-# Private implementation function(s)
-#####################################
-
-#' Copy model from an existing NONMEM model
-#'
-#' Private implementation function called by `copy_model_from()` dispatches.
-#' Create new .mod/ctl and new .yaml files based on a previous model. Used for iterating on model development.
-#' Also fills in necessary YAML fields for using `run_log()` later.
-#' @param .parent_mod S3 object of class `bbi_nonmem_model` to be used as the basis for copy.
-#' @param .update_model_file If `TRUE`, the default, update the `$PROBLEM` line in the new control stream. If `FALSE`, `{.new_model}.[mod|ctl]` will be an exact copy of its parent control stream.
-#' @inheritParams copy_model_from
-#' @importFrom fs file_copy path_rel is_absolute_path
-#' @importFrom readr read_file write_file
-#' @importFrom stringr str_replace
-#' @importFrom yaml write_yaml
-#' @importFrom purrr list_modify
-#' @importFrom digest digest
-#' @return S3 object of class `bbi_nonmem_model` that can be passed to `submit_nonmem_model()`
-#' @keywords internal
-copy_nonmem_model_from <- function(
+#' @describeIn copy_model_from `.parent_mod` takes a `bbi_stan_model` object to use as a basis for the copy.
+#' @export
+copy_model_from.bbi_stan_model <- function(
   .parent_mod,
   .new_model,
   .description = NULL,
@@ -125,23 +108,74 @@ copy_nonmem_model_from <- function(
   .update_model_file = TRUE,
   .overwrite = FALSE
 ) {
-  # Check model for correct class
-  if (!inherits(.parent_mod, NM_MOD_CLASS)) {
-    stop(paste(
-      "copy_nonmem_model_from() requires a model object of class `bbi_nonmem_model`. Passed object has the following classes:",
-      paste(class(.parent_mod), collapse = ", "),
-      "Consider creating a model with `new_model()` or `read_model()`",
-      sep = "\n"))
-  }
 
-  # check parent against YAML
+  check_stan_model(.parent_mod, .error = TRUE)
+
+  .new_model <- build_new_model_path(.parent_mod, .new_model)
+
+  .mod <- copy_model_from_impl(
+    .parent_mod = .parent_mod,
+    .new_model = .new_model,
+    .description = .description,
+    .based_on_additional = .based_on_additional,
+    .add_tags = .add_tags,
+    .inherit_tags = .inherit_tags,
+    .update_model_file = .update_model_file,
+    .overwrite = .overwrite,
+    .model_type = "stan"
+  )
+
+  return(.mod)
+}
+
+#####################################
+# Private implementation function(s)
+#####################################
+
+#' Copy model from an existing model
+#'
+#' Private implementation function called by `copy_model_from()` dispatches.
+#' Create new new .yaml and other necessary files based on a previous model.
+#' Used for iterating on model development. Also fills in necessary YAML fields
+#' for using `run_log()` later.
+#' @param .parent_mod S3 object of class `bbi_{.model_type}_model` to be used as the basis for copy.
+#' @param .update_model_file **Only relevant for NONMEM models.** If `TRUE`, the
+#'   default, update the `$PROBLEM` line in the new control stream. If `FALSE`,
+#'   `{.new_model}.[mod|ctl]` will be an exact copy of its parent control
+#'   stream.
+#' @inheritParams copy_model_from
+#' @importFrom fs file_copy path_rel is_absolute_path
+#' @importFrom readr read_file write_file
+#' @importFrom stringr str_replace
+#' @importFrom yaml write_yaml
+#' @importFrom purrr list_modify
+#' @importFrom digest digest
+#' @return S3 object with the same class as `.parent_mod`
+#' @keywords internal
+copy_model_from_impl <- function(
+  .parent_mod,
+  .new_model,
+  .description = NULL,
+  .based_on_additional = NULL,
+  .add_tags = NULL,
+  .inherit_tags = FALSE,
+  .update_model_file = TRUE,
+  .overwrite = FALSE,
+  .model_type = c("nonmem", "stan")
+) {
+
+  .model_type <- match.arg(.model_type)
+
+  check_for_existing_model(.new_model, .overwrite)
+
+  # check parent
   check_yaml_in_sync(.parent_mod)
 
   # build based_on
   if(!fs::is_absolute_path(.new_model)) {
-    dev_error(".new_model argument to copy_nonmem_model_from() must be absolute.")
+    dev_error(".new_model argument to copy_model_from_impl() must be absolute.")
   }
-  .parent_based_on <- fs::path_rel(get_model_path(.parent_mod), start = dirname(.new_model))
+  .parent_based_on <- fs::path_rel(.parent_mod[[ABS_MOD_PATH]], start = dirname(.new_model))
 
   # build tags
   if (.inherit_tags && !is.null(.parent_mod[[YAML_TAGS]])) {
@@ -150,11 +184,20 @@ copy_nonmem_model_from <- function(
     new_tags <- .add_tags
   }
 
-  # copy control steam to new path
-  .parent_model_path <- get_model_path(.parent_mod)
-  parent_ext <- fs::path_ext(.parent_model_path)
-  .new_model_path <- paste(.new_model, parent_ext, sep = ".")
-  copy_control_stream(.parent_model_path, .new_model_path, .overwrite, .update_model_file)
+  if (inherits(.parent_mod, NM_MOD_CLASS)) {
+    # copy control steam to new path
+    .parent_model_path <- get_model_path(.parent_mod)
+    parent_ext <- fs::path_ext(.parent_model_path)
+    .new_model_path <- paste(.new_model, parent_ext, sep = ".")
+    copy_control_stream(.parent_model_path, .new_model_path, .overwrite, .update_model_file)
+  } else if (inherits(.parent_mod, STAN_MOD_CLASS)) {
+    copy_stan_files(.parent_mod, .new_model, .overwrite)
+  } else {
+    dev_error(paste(
+      "Unsupported object passed to copy_model_from_impl(). Classes: ",
+      paste(class(.parent_mod), collapse = ", ")
+    ))
+  }
 
   # create new model
   .new_mod <- new_model(
@@ -163,8 +206,8 @@ copy_nonmem_model_from <- function(
     .based_on = c(.parent_based_on, .based_on_additional),
     .tags = new_tags,
     .bbi_args = .parent_mod[[YAML_BBI_ARGS]],
-    .overwrite = .overwrite,
-    .model_type = "nonmem"
+    .overwrite = FALSE, # will have already overwritten if necessary
+    .model_type = .model_type
   )
 
   return(.new_mod)
@@ -182,8 +225,13 @@ copy_nonmem_model_from <- function(
 #' @keywords internal
 copy_control_stream <- function(.parent_model_path, .new_model_path, .overwrite, .update_model_file = FALSE) {
 
-  if (fs::file_exists(.new_model_path) && !isTRUE(.overwrite)) {
-    stop(glue("File already exists at {.new_model_path} -- cannot copy new control stream. Either delete old file or use `new_model({yaml_ext(.new_model_path)})`"))
+  if (fs::file_exists(.new_model_path)) {
+    if (isTRUE(.overwrite)) {
+      fs::file_delete(.new_model_path)
+    } else {
+      stop(glue("File already exists at {.new_model_path} -- cannot copy new control stream. Either pass `.overwrite = TRUE` or use `new_model({tools::file_path_sans_ext(.new_model_path)})`"))
+    }
+
   }
 
   if (.update_model_file) {
@@ -203,6 +251,43 @@ copy_control_stream <- function(.parent_model_path, .new_model_path, .overwrite,
   } else {
     fs::file_copy(.parent_model_path, .new_model_path)
   }
+}
+
+
+#' Copy necessary Stan files
+#'
+#' Helper function to copy a necessary Stan files from parent model to new model
+#' and rename them appropriately. Note that any directory existing at
+#' `.new_model` will be overwritten by default.
+#' @param .parent_mod a `bbi_stan_model` object to copy from
+#' @param .new_model Path to new model directory
+#' @param .overwrite If `TRUE`, overwrite existing directory at `.new_model`. If `FALSE` and directory exists at `.new_model` error.
+#' @keywords internal
+copy_stan_files <- function(.parent_mod, .new_model, .overwrite) {
+
+  if (fs::dir_exists(.new_model)) {
+    if (isTRUE(.overwrite)) {
+      fs::dir_delete(.new_model)
+    } else {
+      stop(glue("Directory already exists at {.new_model} -- cannot copy new model files. Either pass `.overwrite = TRUE` or use `new_model({.new_model})`"))
+    }
+  }
+  fs::dir_create(.new_model)
+
+  # can't use real build_path_from_model() because the new model doesn't exist yet
+  build_path_from_new_model_path <- function(.new_model, .suffix) {
+    file.path(
+      .new_model,
+      paste0(basename(.new_model), .suffix)
+    )
+  }
+
+  purrr::walk(STAN_MODEL_REQ_FILES, function(.s) {
+    fs::file_copy(
+      build_path_from_model(.parent_mod, .s),
+      build_path_from_new_model_path(.new_model, .s)
+    )
+  })
 }
 
 #' Private helper to build absolute path for [copy_model_from()].
--- tests/testthat/test-copy-model-from.R
+++ tests/testthat/test-copy-model-from.R
@@ -155,3 +155,17 @@ test_that("copy_model_from() supports `.new_model` containing a period", {
   expect_true(fs::file_exists(new_yaml))
 
 })
+
+test_that("copy_from_model.bbi_stan_model creates accurate copy", {
+  skip_if_no_stan("copy_model_from.bbi_stan_model")
+
+  mod_name <- "testmod_copy_stan1"
+  new_mod <- copy_model_from(STAN_MOD1, mod_name, .inherit_tags = TRUE, .add_tags = NEW_TAGS)
+  on.exit(cleanup_model(new_mod))
+
+  # check that everything is copied through in the object
+  expect_true(inherits(new_mod, STAN_MOD_CLASS))
+  expect_identical(new_mod[[YAML_BASED_ON]], STAN_MOD_ID)
+  expect_identical(new_mod[[YAML_TAGS]], c(STAN_MOD1$tags, NEW_TAGS))
+  expect_null(new_mod[[YAML_DESCRIPTION]])
+})
