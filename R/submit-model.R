455e7a5b93aac5d4c040831364107e26774c8b96
seth127 <seth127@gmail.com>
2021-03-11 17:22:05 -0500

    WIP: first draft of submit_model.bbi_stan_model(). Doesn't yet run because all the helper functions haven't been defined yet.

--- R/submit-model.R
+++ R/submit-model.R
@@ -14,7 +14,10 @@
 #'   global YAML files).
 #' @param .mode Either `"sge"`, the default, to submit model(s) to the grid or
 #'   `"local"` for local execution.
-#' @param ... args passed through to `bbi_exec()`
+#' @param ... args passed through. For `bbi_stan_model` this is how you pass
+#'   arguments through to the `$sample()` method of your `cmdstanr` model
+#'   object. See [cmdstanr::sample()] for valid arguments and details. For
+#'   `bbi_nonmem_model` these are passed to `bbi_exec()`.
 #' @param .overwrite Logical to specify whether or not to overwrite existing
 #'   model output from a previous run. If `NULL`, the default, will defer to
 #'   setting in `.bbi_args` or `bbi.yaml`. If _not_ `NULL` will override any
@@ -66,6 +69,33 @@ submit_model.bbi_nonmem_model <- function(
   return(res)
 }
 
+#' @describeIn submit_model Takes a `bbi_stan_model` object. All arguments
+#'   passed through `...` will be passed to [cmdstanr::sample()] method.
+#' @export
+submit_model.bbi_stan_model <- function(
+  .mod,
+  .bbi_args = NULL,
+  .mode = c("local"),
+  ...,
+  .overwrite = NULL,
+  .config_path = NULL,
+  .wait = NULL,
+  .dry_run=NULL
+) {
+
+  if(!is.null(.bbi_args))    {warning(".bbi_args is not a valid argument for submit_model.bbi_stan_model. Ignoring...")}
+  if(!is.null(.config_path)) {warning(".config_path is not a valid argument for submit_model.bbi_stan_model. Ignoring...")}
+  if(!is.null(.wait))        {warning(".wait is not a valid argument for submit_model.bbi_stan_model. Ignoring...")}
+  if(!is.null(.dry_run))     {warning(".dry_run is not a valid argument for submit_model.bbi_stan_model. Ignoring...")}
+
+  res <- submit_stan_model_cmdstanr(
+    .mod,
+    .mode = .mode,
+    ...,
+    .overwrite = .overwrite
+  )
+  return(res)
+}
 
 #####################################
 # Private implementation function(s)
@@ -122,6 +152,66 @@ submit_nonmem_model <- function(.mod,
 
   # launch model
   res <- bbi_exec(cmd_args, .wait = .wait, .dir = model_dir, ...)
+
+  return(res)
+}
+
+#' Submit a Stan model via cmdstanr
+#'
+#' Private implementation function called by `submit_model()` dispatches.
+#' @param .mod An S3 object of class `bbi_stan_model`
+#' @return The object returned from [cmdstanr::sample()]
+#' @keywords internal
+submit_stan_model_cmdstanr <- function(.mod,
+                                       .mode = c("local"), # TODO: add sge mode for cmdstanr
+                                       ...,
+                                       .overwrite = NULL) {
+
+  # check against YAML
+  check_yaml_in_sync(.mod)
+  check_stan_model(.mod, .error = TRUE)
+
+  # check for valid type arg
+  .mode <- match.arg(.mode)
+
+  out_dir <- get_output_dir(.mod, .check_exists = FALSE)
+  standata_json_path <- build_path_from_model(.mod, STANDATA_JSON_SUFFIX)
+  stanargs_path <- build_path_from_model(.mod, STANARGS_SUFFIX)
+
+  if (fs::dir_exists(out_dir)) {
+    if (isTRUE(.overwrite)) {
+      fs::dir_delete(out_dir)
+      fs::dir_create(out_dir)
+      if(fs::file_exists(standata_json_path)) { fs::file_delete(standata_json_path) }
+      if(fs::file_exists(stanargs_path)) { fs::file_delete(stanargs_path) }
+    } else {
+      stop(glue("{out_dir} already exists. Pass submit_model(..., .overwrite = TRUE) to delete it and re-run the model."), call. = FALSE)
+    }
+  } else {
+    fs::dir_create(out_dir)
+  }
+
+  stanmod <- compile_stanmod(.mod)
+  valid_stanargs <- formalArgs(stanmod$sample)
+
+  # capture args, maybe check against sample(), then write to stanargs.R
+  stanargs <- parse_stanargs(.mod, valid_stanargs, ...)
+
+  # construct input data set and initial estimates
+  standata_list <- build_data(.mod, .out_path = standata_json_path)
+  stanargs[["output_dir"]] <- get_output_dir(.mod)
+  stanargs[["data"]] <- standata_json_path
+  stanargs[["init"]] <- import_stan_init(.mod, .standata = standata_list)
+  rm(standata_list) # once we've passed this to import_stan_init() we don't need it in memory
+
+  # launch model
+  res <- do.call(
+    stanmod$sample,
+    args = stanargs
+  )
+
+  # if successful, write bbi_config.json to disk
+  build_stan_bbi_config(.mod)
 
   return(res)
 }
