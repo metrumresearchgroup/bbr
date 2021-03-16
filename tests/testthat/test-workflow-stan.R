c4157574fcdfc1093d7a90a59210087e9206cdfb
seth127 <seth127@gmail.com>
2021-03-16 11:51:52 -0400

    test: added more test-workflow-stan.R tests, including testing bbi_config.json, sampler_diagnostics(), and reloading the fit object.

--- tests/testthat/test-workflow-stan.R
+++ tests/testthat/test-workflow-stan.R
@@ -31,18 +31,49 @@ fs::file_copy(yaml_ext(STAN_MOD1_PATH), MODEL_DIR_STAN_TEST)
 test_that("step by step create_model to submit_model to model_summary works", {
   # create model
   mod1 <- read_model(file.path(MODEL_DIR_STAN_TEST, STAN_MOD_ID))
-  mod2 <- copy_model_from(mod1, "fxa2", .add_tags = "child")
+  mod2 <- copy_model_from(mod1, STAN_MOD_ID2, .add_tags = "child")
 
   # submit model
-  res2 <- submit_model(
-    mod2,
-    .mode = "local",
-    .overwrite = TRUE,
-    iter_warmup = 100,
-    iter_sampling = 100
+  res_output <- capture.output(
+    res2 <- submit_model(
+      mod2,
+      .mode = "local",
+      .overwrite = TRUE,
+      iter_warmup = 100,
+      iter_sampling = 100
+    )
   )
   expect_true(inherits(res2, "CmdStanMCMC"))
 
+  # check sampler diagnostics returns draws array
+  smp2 <- res2$sampler_diagnostics()
+  expect_true(inherits(smp2, "draws_array"))
+  expect_equal(dim(smp2), c(100, 4, 6))
+
+  # check the output for mention of all the chains
+  expect_true(all(
+    purrr::map_lgl(paste("Chain", 1:4), ~any(str_detect(res_output, .x)))
+  ))
+
+  # saves a fit object
+  expect_true(fs::file_exists(build_path_from_model(mod2, STAN_MODEL_FIT_RDS)))
+
+  # saves a config with some md5 hashes in it
+  cfg_res <- jsonlite::fromJSON(file.path(get_output_dir(mod2), "bbi_config.json"))
+  expect_true(any(str_detect(names(cfg_res), "md5$")))
+
+})
+
+test_that("cmdstanr fit object can be reloaded", {
+  res2 <- read_model(file.path(MODEL_DIR_STAN_TEST, STAN_MOD_ID2)) %>%
+    build_path_from_model(STAN_MODEL_FIT_RDS) %>%
+    readRDS()
+  expect_true(inherits(res2, "CmdStanMCMC"))
+
+  # verify the sampler_diagnostics() method works
+  smp2 <- res2$sampler_diagnostics()
+  expect_true(inherits(smp2, "draws_array"))
+  expect_equal(dim(smp2), c(100, 4, 6))
 })
 
 test_that("run_log() captures runs correctly", {
@@ -50,5 +81,5 @@ test_that("run_log() captures runs correctly", {
   log_df <- run_log(MODEL_DIR_STAN_TEST)
   expect_equal(nrow(log_df), 2)
   expect_equal(ncol(log_df), RUN_LOG_COLS)
-  expect_identical(basename(log_df[[ABS_MOD_PATH]]), c(STAN_MOD_ID, "fxa2"))
+  expect_identical(basename(log_df[[ABS_MOD_PATH]]), c(STAN_MOD_ID, STAN_MOD_ID2))
 })
