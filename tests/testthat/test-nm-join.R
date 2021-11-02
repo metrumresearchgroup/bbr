bfb6eb2df69ae7bc2e250aee471b748bd0680695
seth127 <seth127@gmail.com>
2021-11-02 17:15:31 -0400

    test: fixing columns and rows in first round of nm_join() tests

--- tests/testthat/test-nm-join.R
+++ tests/testthat/test-nm-join.R
@@ -2,6 +2,12 @@ context("Testing nm_join()")
 
 skip_if_not_drone_or_metworx("test-model-summary")
 
+DATA_ROWS <- 799
+JOIN_ROWS <- 779
+DATA_COLS <- 10
+TAB_NEW_COLS <- 7
+PARTAB_NEW_COLS <- 5
+
 withr::with_options(list(
   bbr.bbi_exe_path = read_bbi_path(),
   bbr.verbose = FALSE
@@ -12,33 +18,34 @@ withr::with_options(list(
       expect_message({
         test_df <- nm_join(MOD1)
       }, regexp = "data file")
-      expect_equal(nrow(test_df), 779)
-      expect_equal(ncol(test_df), 20)
+      expect_equal(nrow(test_df), JOIN_ROWS)
+      expect_equal(ncol(test_df), DATA_COLS + TAB_NEW_COLS + PARTAB_NEW_COLS)
     })
   })
 
   test_that("nm_join() works correctly with file path", {
     test_df <- nm_join(MOD1_ABS_PATH)
-    expect_equal(nrow(test_df), 779)
-    expect_equal(ncol(test_df), 20)
+    expect_equal(nrow(test_df), JOIN_ROWS)
+    expect_equal(ncol(test_df), DATA_COLS + TAB_NEW_COLS + PARTAB_NEW_COLS)
   })
 
   test_that("nm_join(.superset) works correctly", {
     test_df <- nm_join(MOD1, .superset = TRUE)
-    expect_equal(nrow(test_df), 799)
-    expect_equal(ncol(test_df), 20)
+    expect_equal(nrow(test_df), DATA_ROWS)
+    expect_equal(ncol(test_df), DATA_COLS + TAB_NEW_COLS + PARTAB_NEW_COLS)
     expect_equal(sum(is.na(test_df$PRED)), 20)
   })
 
-  #### first only tests
+  ####################
+  # first only tests
 
   test_that("nm_join(.files) works correctly FIRSTONLY with ID col", {
     test_df <- nm_join(
       MOD1,
       .files = build_path_from_model(.mod, "first1.tab")
     )
-    expect_equal(nrow(test_df), 779)
-    expect_equal(ncol(test_df), 11)
+    expect_equal(nrow(test_df), JOIN_ROWS)
+    expect_equal(ncol(test_df), DATA_COLS + 1)
   })
 
   test_that("nm_join(.files) works correctly FIRSTONLY with no ID col", {
@@ -46,8 +53,8 @@ withr::with_options(list(
       MOD1,
       .files = build_path_from_model(.mod, "first2.tab")
     )
-    expect_equal(nrow(test_df), 779)
-    expect_equal(ncol(test_df), 11)
+    expect_equal(nrow(test_df), JOIN_ROWS)
+    expect_equal(ncol(test_df), DATA_COLS + 1)
   })
 
   test_that("nm_join(.files) works correctly FIRSTONLY with both NUM and ID cols", {
@@ -55,8 +62,8 @@ withr::with_options(list(
       MOD1,
       .files = build_path_from_model(.mod, "first3.tab")
     )
-    expect_equal(nrow(test_df), 779)
-    expect_equal(ncol(test_df), 11)
+    expect_equal(nrow(test_df), JOIN_ROWS)
+    expect_equal(ncol(test_df), DATA_COLS + 1)
   })
 
   test_that("nm_join(.files) works correctly FIRSTONLY then full table", {
@@ -67,29 +74,31 @@ withr::with_options(list(
         build_path_from_model(.mod, ".tab")
       )
     )
-    expect_equal(nrow(test_df), 779)
-    expect_equal(ncol(test_df), 18)
+    expect_equal(nrow(test_df), JOIN_ROWS)
+    expect_equal(ncol(test_df), DATA_COLS + 1 + TAB_NEW_COLS)
   })
 
-  #####
+  ######################
+  # duplicate columns tests
 
-  test_that("nm_join(.files) works correctly duplicate cols", {
+  test_that("nm_join() works correctly duplicate cols", {
     test_df <- nm_join(
       MOD1,
       .more = build_path_from_model(MOD1, "dups.tab")
     )
-    expect_equal(nrow(test_df), 779)
-    expect_equal(ncol(test_df), 11) # TODO: is this right?
+    expect_equal(nrow(test_df), JOIN_ROWS)
+    expect_equal(ncol(test_df), DATA_COLS + TAB_NEW_COLS + PARTAB_NEW_COLS + 1)
+    expect_equal(test_df$DV.DATA, test_df$DV)
   })
 
   test_that("nm_join(.more) works correctly", {
     test_df <- nm_join(
       MOD1,
       .files = build_path_from_model(MOD1, ".tab"),
-      .more = build_path_from_model(MOD1, "cl.tab")
+      .more = build_path_from_model(MOD1, "first1.tab")
     )
     expect_equal(nrow(test_df), 779)
-    expect_equal(ncol(test_df), 17) # TODO: is this right?
+    expect_equal(ncol(test_df), DATA_COLS + TAB_NEW_COLS + 1)
   })
 
 
@@ -109,23 +118,23 @@ withr::with_options(list(
     expect_warning({
       test_df <- nm_join(MOD1, .files = .tf)
     }, regexp = "skipped because nrow")
-    expect_equal(nrow(test_df), 799)
-    expect_equal(ncol(test_df), 10)
+    expect_equal(nrow(test_df), DATA_ROWS)
+    expect_equal(ncol(test_df), DATA_COLS)
   })
 
   test_that("nm_join() warns on missing file", {
     expect_warning({
       test_df <- nm_join(MOD1, .more = "naw")
     }, regexp = "table files do not exist")
-    expect_equal(nrow(test_df), 779)
-    expect_equal(ncol(test_df), 20)
+    expect_equal(nrow(test_df), JOIN_ROWS)
+    expect_equal(ncol(test_df), DATA_COLS + TAB_NEW_COLS + PARTAB_NEW_COLS)
   })
 
   test_that("nm_join() warns on no files found", {
     expect_warning({
       test_df <- nm_join(MOD1, .files = "naw")
     }, regexp = "[Zz]ero table files found")
-    expect_equal(nrow(test_df), 799)
-    expect_equal(ncol(test_df), 10)
+    expect_equal(nrow(test_df), DATA_ROWS)
+    expect_equal(ncol(test_df), DATA_COLS)
   })
 })
