954423e84e01caff2242315c9e35906a8fc885f9
seth127 <seth127@gmail.com>
2021-11-03 16:10:03 -0400

    test: adding initial test-nm-file.R tests.
    # WIP: STILL NEED TO TEST
    # * nm_tab()
    # * nm_par_tab()

---
+++ tests/testthat/test-nm-file.R
@@ -0,0 +1,59 @@
+
+MOD1_PARAMS <- 9
+
+# WIP: STILL NEED TO TEST
+# * nm_tab()
+# * nm_par_tab()
+
+test_that("nm_file() works with .cov", {
+  .d <- nm_file(MOD1, ".cov")
+  expect_equal(ncol(.d), MOD1_PARAMS+1)
+  expect_equal(nrow(.d), MOD1_PARAMS)
+})
+
+test_that("nm_ext() works", {
+  .d <- nm_ext(MOD1)
+  expect_equal(ncol(.d), MOD1_PARAMS+2)
+  expect_equal(nrow(.d), 14)
+})
+
+test_that("nm_grd() works", {
+  .d <- nm_grd(MOD1)
+  expect_equal(ncol(.d), 8)
+  expect_equal(nrow(.d), 7)
+})
+
+test_that("nm_file(.est_method) works", {
+  .m <- read_model(file.path(MODEL_DIR_X,"example2_saemimp"))
+  .d <- nm_file(.m, ".ext")
+  .d1 <- nm_file(.m, ".ext", 1)
+  .d2 <- nm_file(.m, ".ext", 2)
+
+  expect_equal(.d, .d2)
+  expect_true(ncol(.d) == ncol(.d1))
+  expect_false(nrow(.d) == nrow(.d1))
+})
+
+test_that("nm_ext(.est_method) works", {
+  .m <- read_model(file.path(MODEL_DIR_X,"example2_saemimp"))
+  .d <- nm_ext(.m)
+  .d1 <- nm_ext(.m, 1)
+  .d2 <- nm_ext(.m, 2)
+
+  expect_equal(.d, .d2)
+  expect_true(ncol(.d) == ncol(.d1))
+  expect_false(nrow(.d) == nrow(.d1))
+})
+
+test_that("nm_file(.est_method) errors with invalid .est_method", {
+  expect_error(nm_ext(.m, 3))
+})
+
+test_that("nm_data() works", {
+  expect_message({
+    .d <- nm_data(MOD1)
+  }, regexp = "Reading.+acop")
+
+  expect_equal(ncol(.d), 9)
+  expect_equal(nrow(.d), 799)
+})
