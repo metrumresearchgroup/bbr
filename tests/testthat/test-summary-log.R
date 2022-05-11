97fc06f74e87fdb73f8c98769251a0439ad5e743
rymarinelli <ram478@drexel.edu>
2022-05-11 14:41:01 -0400

    documentation: Updating docs for exclude

--- tests/testthat/test-summary-log.R
+++ tests/testthat/test-summary-log.R
@@ -164,9 +164,9 @@ test_that("summary_log() works with filtering parameter numeric [BBR-SMLG-011]",
             copy_all_output_dirs()
             log_df <- list(df = summary_log(MODEL_DIR), length = summary_log(MODEL_DIR) %>% nrow())
 
-            expect_equal(summary_log(MODEL_DIR, .filter = 1:(log_df$length - 1)) %>% nrow(), 1)
-            expect_equal(summary_log(MODEL_DIR, .filter = 1:(log_df$length - 2)) %>% nrow(), 2)
-            expect_equal(summary_log(MODEL_DIR, .filter = (log_df$length - 2):1) %>% nrow(), 2)
+            expect_equal(summary_log(MODEL_DIR, .exclude = 1:(log_df$length - 1)) %>% nrow(), 1)
+            expect_equal(summary_log(MODEL_DIR, .exclude = 1:(log_df$length - 2)) %>% nrow(), 2)
+            expect_equal(summary_log(MODEL_DIR, .exclude = (log_df$length - 2):1) %>% nrow(), 2)
           })
 
 test_that("summary_log() works with filtering parameter string [BBR-SMLG-011]",
@@ -187,10 +187,10 @@ test_that("summary_log() works with filtering parameter string [BBR-SMLG-011]",
 
           log_df <- list(df = summary_log(MODEL_DIR), length = summary_log(MODEL_DIR) %>% nrow())
 
-          expect_equal(summary_log(MODEL_DIR, .filter = c(1:2, "Child")) %>% nrow(),2)
-          expect_equal(summary_log(MODEL_DIR, .filter = c(2:1, "Child")) %>% nrow() ,2)
-          expect_equal(summary_log(MODEL_DIR, .filter = c("Child", 1, 2, 3)) %>% nrow() ,1)
-          expect_equal(summary_log(MODEL_DIR, .filter = c(1:2, "Parent")) %>% nrow() ,2)
+          expect_equal(summary_log(MODEL_DIR, .exclude = c(1:2, "Child")) %>% nrow(),2)
+          expect_equal(summary_log(MODEL_DIR, .exclude = c(2:1, "Child")) %>% nrow() ,2)
+          expect_equal(summary_log(MODEL_DIR, .exclude = c("Child", 1, 2, 3)) %>% nrow() ,1)
+          expect_equal(summary_log(MODEL_DIR, .exclude = c(1:2, "Parent")) %>% nrow() ,2)
 
           dir_delete("{MODEL_DIR}/Parent" %>% glue())
           dir_delete("{MODEL_DIR}/Child" %>% glue())
