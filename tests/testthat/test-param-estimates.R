11b47e85dd089495eff6fa84d4237902969a8d04
barrettk <barrettk@metrumrg.com>
2022-06-16 16:24:58 -0400

    fixed requirement ids

--- tests/testthat/test-param-estimates.R
+++ tests/testthat/test-param-estimates.R
@@ -26,7 +26,7 @@ withr::with_options(list(bbr.bbi_exe_path = read_bbi_path()), {
     )
   })
 
-  test_that("param_estimates correctly errors on Bayesian model with multiple estimation methods [BBR-PEST-003]", {
+  test_that("param_estimates correctly errors on Bayesian model with multiple estimation methods [BBR-PEST-002]", {
     clean_test_enviroment()
 
     sum1 <- file.path(MODEL_DIR_X, "acop-fake-bayes") %>%
@@ -38,7 +38,7 @@ withr::with_options(list(bbr.bbi_exe_path = read_bbi_path()), {
     )
   })
 
-  test_that("param_estimates correctly errors on ONLYSIM models [BBR-PEST-003]", {
+  test_that("param_estimates correctly errors on ONLYSIM models [BBR-PEST-002]", {
     clean_test_enviroment()
 
     skip_if_old_bbi("3.1.0")
