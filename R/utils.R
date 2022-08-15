7a8b885a8befe28815ce3ff836218f65a9b5b8d7
rymarinelli <ram478@drexel.edu>
2022-08-15 09:29:50 -0400

    Reset HEAD to before drone failure
    Added comment to utils.R,
    
    Tried to reinstall with pkgr

--- R/utils.R
+++ R/utils.R
@@ -650,7 +650,7 @@ remove_dup_cols <- function(data){
     dup_cols <- names(data)[duplicated(names(data))]
     dup_cols_str <- paste(dup_cols, collapse = ", ")
     dup_id <- c(which(duplicated(names(data))) -1, which(duplicated(names(data)))) %>% sort()
-    warning(glue("Following duplicated columns: {dup_cols_str}\n  Duplicate names will be repaired with `make.unique()`")) # something like this, and then say if they're equal
+    warning(glue("Following duplicated columns: {dup_cols_str}\n  Duplicate names will be repaired with `make.unique()`")) # If colnames are equal, throw warning
     colnames(data) <- make.unique(colnames(data) )
   }
   return(data)
