dc79981ec89e0fa8696a28c027905e323a06d3f6
Kyle Meyer <kylem@metrumrg.com>
2023-01-03 10:43:32 -0500

    copy file_edit wrapper from bbr.bayes
    This is from bbr.bayes commit f0a3f9a.
    
    The purpose of this wrapper is to prevent 'R CMD check' from
    complaining about a bare file.edit() ("Undefined global functions or
    variables") while also keeping RStudio's special handling that
    wouldn't be triggered by calling utils::file.edit() directly.

---
+++ R/open.R
@@ -0,0 +1,13 @@
+
+# Under RStudio, there is a distinction between a plain file.edit() and
+# utils::file.edit(). RStudio overrides file.edit() so that it opens up a tab.
+# utils::file.edit(), on the other hand, pops up a window that the user must
+# save before returning to RStudio.
+file_edit <- function(...) {
+  fn <- if (exists("file.edit", envir = globalenv())) {
+    get("file.edit", envir = globalenv())
+  } else {
+    utils::file.edit
+  }
+  fn(...)
+}
