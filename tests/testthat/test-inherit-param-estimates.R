e2344a2970ae205d4a2a06f09ddd93dbf4d9432e
Kyle Barrett <barrettk@metrumrg.com>
2023-12-07 14:34:33 -0500

    test fix: ensure bbi path can be found.
     - inherit estimates requires bbi path to be set because of the underlying model_summary() call

--- tests/testthat/test-inherit-param-estimates.R
+++ tests/testthat/test-inherit-param-estimates.R
@@ -36,108 +36,110 @@ get_param_inits <- function(.mod, init_only = TRUE){
   return(recs)
 }
 
-describe("inherit_param_estimates: integration", {
+withr::with_options(list(bbr.bbi_exe_path = read_bbi_path()), {
+  describe("inherit_param_estimates: integration", {
 
-  it("base model", {
-    # errors if no based_on field
-    expect_error(inherit_param_estimates(MOD1), "did not return any parent models")
+    it("base model", {
+      # errors if no based_on field
+      expect_error(inherit_param_estimates(MOD1), "did not return any parent models")
 
-    # Normal behavior
-    mod_est <- copy_model_from(MOD1, "mod_est", "Inherit estimates", .overwrite = TRUE) %>%
-      inherit_param_estimates()
-    on.exit(delete_models(mod_est, .tags = NULL, .force = TRUE))
+      # Normal behavior
+      mod_est <- copy_model_from(MOD1, "mod_est", "Inherit estimates", .overwrite = TRUE) %>%
+        inherit_param_estimates()
+      on.exit(delete_models(mod_est, .tags = NULL, .force = TRUE))
 
-    mod1_params_final <- list(
-      thetas = SUM1 %>% get_theta() %>% sprintf("%.3G", .),
-      # Only grab diagonals since no block matrices used
-      omegas = diag(SUM1 %>% get_omega()) %>% sprintf("%.3G", .),
-      sigmas = diag(SUM1 %>% get_sigma()) %>% sprintf("%.3G", .)
-    )
+      mod1_params_final <- list(
+        thetas = SUM1 %>% get_theta() %>% sprintf("%.3G", .),
+        # Only grab diagonals since no block matrices used
+        omegas = diag(SUM1 %>% get_omega()) %>% sprintf("%.3G", .),
+        sigmas = diag(SUM1 %>% get_sigma()) %>% sprintf("%.3G", .)
+      )
 
-    mod2_inits_inherit <- get_param_inits(mod_est)
+      mod2_inits_inherit <- get_param_inits(mod_est)
 
-    expect_equal(mod1_params_final$thetas, mod2_inits_inherit$thetas[[1]])
-    expect_equal(mod1_params_final$omegas, mod2_inits_inherit$omegas[[1]])
-    expect_equal(mod1_params_final$sigmas, mod2_inits_inherit$sigmas[[1]])
+      expect_equal(mod1_params_final$thetas, mod2_inits_inherit$thetas[[1]])
+      expect_equal(mod1_params_final$omegas, mod2_inits_inherit$omegas[[1]])
+      expect_equal(mod1_params_final$sigmas, mod2_inits_inherit$sigmas[[1]])
 
-    # Confirm theta bounds
-    mod2_params_inherit <- get_param_inits(mod_est, init_only = FALSE)
-    expect_equal(
-      mod2_params_inherit$thetas[[1]][1:3],
-      c("(0, 2.32)", "(0, 54.6)", "(0, 463)")
-    )
+      # Confirm theta bounds
+      mod2_params_inherit <- get_param_inits(mod_est, init_only = FALSE)
+      expect_equal(
+        mod2_params_inherit$thetas[[1]][1:3],
+        c("(0, 2.32)", "(0, 54.6)", "(0, 463)")
+      )
+    })
+
+    it("base model - revert theta bounds", {
+      mod_est <- copy_model_from(MOD1, "mod_est", "Inherit estimates", .overwrite = TRUE) %>%
+        inherit_param_estimates(.bounds = "discard")
+      on.exit(delete_models(mod_est, .tags = NULL, .force = TRUE))
+
+      mod1_params_final <- list(
+        thetas = SUM1 %>% get_theta() %>% sprintf("%.3G", .)
+      )
+
+      # Confirm theta bounds - parens removed where bounds are removed
+      mod2_params_inherit <- get_param_inits(mod_est, init_only = FALSE)
+      expect_equal(mod1_params_final$thetas[1:3], mod2_params_inherit$thetas[[1]][1:3])
+    })
+
+    it("pass a different model", {
+      mod_est <- copy_model_from(MOD1, "mod_est", .overwrite = TRUE)
+      mod_est2 <- copy_model_from(mod_est, "mod_est2", .overwrite = TRUE)
+      on.exit(delete_models(list(mod_est, mod_est2), .tags = NULL, .force = TRUE))
+
+      expect_error(inherit_param_estimates(mod_est2), "has not been executed")
+
+      mod_est2 <- inherit_param_estimates(mod_est2, .parent_mod = MOD1$absolute_model_path)
+
+      mod1_params_final <- list(
+        thetas = SUM1 %>% get_theta() %>% sprintf("%.3G", .),
+        # Only grab diagonals since no block matrices used
+        omegas = diag(SUM1 %>% get_omega()) %>% sprintf("%.3G", .),
+        sigmas = diag(SUM1 %>% get_sigma()) %>% sprintf("%.3G", .)
+      )
+
+      mod2_inits_inherit <- get_param_inits(mod_est2)
+
+      expect_equal(mod1_params_final$thetas, mod2_inits_inherit$thetas[[1]])
+      expect_equal(mod1_params_final$omegas, mod2_inits_inherit$omegas[[1]])
+      expect_equal(mod1_params_final$sigmas, mod2_inits_inherit$sigmas[[1]])
+
+      # Ensure model objects can also be passed
+      expect_no_error(inherit_param_estimates(mod_est2, .parent_mod = MOD1))
+    })
+
+    it("Inheriting only some parameters", {
+      mod_est <- copy_model_from(MOD1, "mod_est", "Inherit estimates", .overwrite = TRUE) %>%
+        inherit_param_estimates(.inherit = c("theta"))
+      on.exit(delete_models(mod_est, .tags = NULL, .force = TRUE))
+
+      mod1_params_final <- list(
+        thetas = SUM1 %>% get_theta() %>% sprintf("%.3G", .),
+        # Only grab diagonals since no block matrices used
+        omegas = diag(SUM1 %>% get_omega()) %>% sprintf("%.3G", .),
+        sigmas = diag(SUM1 %>% get_sigma()) %>% sprintf("%.3G", .)
+      )
+
+      mod2_inits_inherit <- get_param_inits(mod_est)
+
+      # thetas changed
+      expect_equal(mod1_params_final$thetas, mod2_inits_inherit$thetas[[1]])
+      # Confirm omegas didnt change
+      expect_equal(mod1_params_final$omegas, c("0.0985", "0.157"))
+      expect_equal(mod2_inits_inherit$omegas[[1]], c("0.05", "0.2"))
+    })
+
+    it("fails with old method of using priors", {
+      mod <- read_model(file.path(MODEL_DIR_X, "example2_saemimp"))
+      mod_est <- copy_model_from(mod, "mod_est", "Inherit estimates", .overwrite = TRUE)
+      on.exit(delete_models(mod_est, .tags = NULL, .force = TRUE))
+
+      expect_error(
+        inherit_param_estimates(mod_est),
+        "If you're using THETA records for priors"
+      )
+    })
   })
 
-  it("base model - revert theta bounds", {
-    mod_est <- copy_model_from(MOD1, "mod_est", "Inherit estimates", .overwrite = TRUE) %>%
-      inherit_param_estimates(.bounds = "discard")
-    on.exit(delete_models(mod_est, .tags = NULL, .force = TRUE))
-
-    mod1_params_final <- list(
-      thetas = SUM1 %>% get_theta() %>% sprintf("%.3G", .)
-    )
-
-    # Confirm theta bounds - parens removed where bounds are removed
-    mod2_params_inherit <- get_param_inits(mod_est, init_only = FALSE)
-    expect_equal(mod1_params_final$thetas[1:3], mod2_params_inherit$thetas[[1]][1:3])
-  })
-
-  it("pass a different model", {
-    mod_est <- copy_model_from(MOD1, "mod_est", .overwrite = TRUE)
-    mod_est2 <- copy_model_from(mod_est, "mod_est2", .overwrite = TRUE)
-    on.exit(delete_models(list(mod_est, mod_est2), .tags = NULL, .force = TRUE))
-
-    expect_error(inherit_param_estimates(mod_est2), "has not been executed")
-
-    mod_est2 <- inherit_param_estimates(mod_est2, .parent_mod = MOD1$absolute_model_path)
-
-    mod1_params_final <- list(
-      thetas = SUM1 %>% get_theta() %>% sprintf("%.3G", .),
-      # Only grab diagonals since no block matrices used
-      omegas = diag(SUM1 %>% get_omega()) %>% sprintf("%.3G", .),
-      sigmas = diag(SUM1 %>% get_sigma()) %>% sprintf("%.3G", .)
-    )
-
-    mod2_inits_inherit <- get_param_inits(mod_est2)
-
-    expect_equal(mod1_params_final$thetas, mod2_inits_inherit$thetas[[1]])
-    expect_equal(mod1_params_final$omegas, mod2_inits_inherit$omegas[[1]])
-    expect_equal(mod1_params_final$sigmas, mod2_inits_inherit$sigmas[[1]])
-
-    # Ensure model objects can also be passed
-    expect_no_error(inherit_param_estimates(mod_est2, .parent_mod = MOD1))
-  })
-
-  it("Inheriting only some parameters", {
-    mod_est <- copy_model_from(MOD1, "mod_est", "Inherit estimates", .overwrite = TRUE) %>%
-      inherit_param_estimates(.inherit = c("theta"))
-    on.exit(delete_models(mod_est, .tags = NULL, .force = TRUE))
-
-    mod1_params_final <- list(
-      thetas = SUM1 %>% get_theta() %>% sprintf("%.3G", .),
-      # Only grab diagonals since no block matrices used
-      omegas = diag(SUM1 %>% get_omega()) %>% sprintf("%.3G", .),
-      sigmas = diag(SUM1 %>% get_sigma()) %>% sprintf("%.3G", .)
-    )
-
-    mod2_inits_inherit <- get_param_inits(mod_est)
-
-    # thetas changed
-    expect_equal(mod1_params_final$thetas, mod2_inits_inherit$thetas[[1]])
-    # Confirm omegas didnt change
-    expect_equal(mod1_params_final$omegas, c("0.0985", "0.157"))
-    expect_equal(mod2_inits_inherit$omegas[[1]], c("0.05", "0.2"))
-  })
-
-  it("fails with old method of using priors", {
-    mod <- read_model(file.path(MODEL_DIR_X, "example2_saemimp"))
-    mod_est <- copy_model_from(mod, "mod_est", "Inherit estimates", .overwrite = TRUE)
-    on.exit(delete_models(mod_est, .tags = NULL, .force = TRUE))
-
-    expect_error(
-      inherit_param_estimates(mod_est),
-      "If you're using THETA records for priors"
-    )
-  })
 })
-
