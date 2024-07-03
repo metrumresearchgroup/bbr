18232f15d2b73bbd3edc4bd34dfc4ef756e4359e
Kyle Barrett <barrettk@metrumrg.com>
2024-07-03 19:03:46 -0400

    extend support for null mapping and dropping of columns
     - After some internal discussion, it was suggested that the ability to create a full NM-TRAN dataset via parsing the $DATA and $INPUT records could be valuable.
    
     - This commit adds the dropping of columns via `DROP` and `SKIP` options, handles the renaming of columns, and null mapping (changing null values to a particular character string).
    
     - nm_data_filter was renamed from filter_nm_data. All the helper function names may change, but was trying to be consistent across the new helper functions at the minimum.
    
     - minor bug fix to nm_data_filter: can handle `=` IGNORE/ACCEPT options

--- R/modify-records.R
+++ R/modify-records.R
@@ -195,22 +195,12 @@ modify_data_path_ctl <- function(.mod, data_path){
   ctl <- get_model_ctl(.mod)
 
   # Get data record
-  data_recs <- nmrec::select_records(ctl, "data")
-  n_data <- length(data_recs)
-  if(n_data !=1){
-    recs_fmt <- purrr::map_chr(data_recs, function(rec) rec$format())
-    rlang::abort(
-      c(
-        glue::glue("Expected a single data record, but found {n_data}:\n\n"),
-        recs_fmt
-      )
-    )
-  }
+  data_rec <- read_data_record(.mod)
 
   # Overwrite 'filename' option
-  filename_opt <- nmrec::get_record_option(data_recs[[1]], "filename")
+  filename_opt <- nmrec::get_record_option(data_rec, "filename")
   if (is.null(filename_opt)) {
-    dev_error(glue("No filename option found in `$DATA` record: \n{data_recs[[1]]$format()}"))
+    dev_error(glue("No filename option found in `$DATA` record: \n{data_rec$format()}"))
   }
   filename_opt$value <- data_path
 
@@ -324,39 +314,9 @@ get_table_columns <- function(.mod, from_data = TRUE){
 }
 
 
-# Function to translate NONMEM operators to R operators
-translate_operator <- function(expr) {
-  expr <- gsub("\\.EQ\\.", "==", expr)
-  expr <- gsub("\\.NE\\.", "!=", expr)
-  expr <- gsub("\\.LT\\.", "<", expr)
-  expr <- gsub("\\.LE\\.", "<=", expr)
-  expr <- gsub("\\.GT\\.", ">", expr)
-  expr <- gsub("\\.GE\\.", ">=", expr)
-  return(expr)
-}
-
-# Function to invert an expression (for ignore statements)
-invert_expression <- function(expr) {
-  expr <- dplyr::case_when(
-    grepl("==", expr) ~ gsub("==", "!=", expr),
-    grepl("!=", expr) ~ gsub("!=", "==", expr),
-    grepl("<=", expr) ~ gsub("<=", ">=", expr),
-    grepl(">=", expr) ~ gsub(">=", "<=", expr),
-    grepl("<", expr) ~ gsub("<", ">", expr),
-    grepl(">", expr) ~ gsub(">", "<", expr),
-    TRUE ~ expr
-  )
-  return(expr)
-}
-
-
-#' Filter `NONMEM` input data based on `IGNORE` and `ACCEPT` record options
-#'
-#' @param .mod a `bbi_nonmem_model` object
-#' @param data a starting dataset
-#' @keywords internal
-filter_nm_data <- function(.mod, data = nm_data(.mod)){
-
+#' Helper for reading in and parsing the `$DATA` record.
+#' @noRd
+read_data_record <- function(.mod){
   data_recs <- get_records(.mod, "data")
   n_data <- length(data_recs)
   if(n_data !=1){
@@ -369,9 +329,47 @@ filter_nm_data <- function(.mod, data = nm_data(.mod)){
     )
   }
   data_recs[[1]]$parse()
+  return(data_recs[[1]])
+}
+
+
+
+#' Filter `NONMEM` input data based on `IGNORE` and `ACCEPT` record options
+#'
+#' @param .mod a `bbi_nonmem_model` object
+#' @param data a starting dataset
+#' @keywords internal
+nm_data_filter <- function(.mod, data = nm_data(.mod)){
+
+  # Function to translate NONMEM operators to R operators
+  translate_nm_operator <- function(expr) {
+    expr <- gsub("\\.EQ\\.|=", "==", expr)
+    expr <- gsub("\\.NE\\.", "!=", expr)
+    expr <- gsub("\\.LT\\.", "<", expr)
+    expr <- gsub("\\.LE\\.", "<=", expr)
+    expr <- gsub("\\.GT\\.", ">", expr)
+    expr <- gsub("\\.GE\\.", ">=", expr)
+    return(expr)
+  }
+
+  # Function to invert an expression (for ignore statements)
+  invert_expression <- function(expr) {
+    expr <- dplyr::case_when(
+      grepl("==", expr) ~ gsub("==", "!=", expr),
+      grepl("!=", expr) ~ gsub("!=", "==", expr),
+      grepl("<=", expr) ~ gsub("<=", ">=", expr),
+      grepl(">=", expr) ~ gsub(">=", "<=", expr),
+      grepl("<", expr) ~ gsub("<", ">", expr),
+      grepl(">", expr) ~ gsub(">", "<", expr),
+      TRUE ~ expr
+    )
+    return(expr)
+  }
+
+  data_rec <- read_data_record(.mod)
 
   # Extract & format IGNORE options
-  ignore_opts <- purrr::keep(data_recs[[1]]$values, function(val){
+  ignore_opts <- purrr::keep(data_rec$values, function(val){
     inherits(val, "nmrec_option_value") && identical(val[["name"]], "ignore")
   })
   ignore_vals <- purrr::map_chr(ignore_opts, function(ign_val){
@@ -379,7 +377,7 @@ filter_nm_data <- function(.mod, data = nm_data(.mod)){
   })
 
   # Extract & format ACCEPT options
-  accept_opts <- purrr::keep(data_recs[[1]]$values, function(val){
+  accept_opts <- purrr::keep(data_rec$values, function(val){
     inherits(val, "nmrec_option_value") && identical(val[["name"]], "accept")
   })
   accept_vals <- purrr::map_chr(accept_opts, function(acc_val){
@@ -388,11 +386,11 @@ filter_nm_data <- function(.mod, data = nm_data(.mod)){
 
 
   # Translate the ignore and accept expressions
-  ignore_exprs <- lapply(ignore_vals, translate_operator) %>% gsub(",", " &", .)
-  accept_exprs <- lapply(accept_vals, translate_operator) %>% gsub(",", " &", .)
+  ignore_exprs <- lapply(ignore_vals, translate_nm_operator) %>% gsub(",", " &", .)
+  accept_exprs <- lapply(accept_vals, translate_nm_operator) %>% gsub(",", " &", .)
 
   ## Combine the expressions into filter expressions ##
-  data_cols <- get_input_columns(.mod)
+  data_cols <- names(data)
 
   # `IGNORE=#`, `IGNORE=@`, `IGNORE=c1`, `IGNORE=(list)`
   ignore_filters <- purrr::map_chr(ignore_exprs, function(expr) {
@@ -423,19 +421,16 @@ filter_nm_data <- function(.mod, data = nm_data(.mod)){
     }
   })
 
-  # Only supports `ACCEPT=(list)` form
-  accept_filters <- purrr::map_chr(accept_exprs, function(expr) {
-    return(paste0(expr))
-  })
+  # ACCEPT option only supports `ACCEPT=(list)` form --> no formatting needed
 
   # Combine all filter expressions
-  all_filters <- c(ignore_filters, accept_filters)
+  all_filters <- c(ignore_filters, accept_exprs)
 
   # Create the final dplyr::filter expression
   filter_expression <- paste(all_filters, collapse = " & ")
 
   # Apply filters
-  tryCatch({
+  filtered_data <- tryCatch({
     data %>% dplyr::filter(eval(parse(text = filter_expression)))
   }, error = function(cond){
     rlang::abort(
@@ -446,6 +441,113 @@ filter_nm_data <- function(.mod, data = nm_data(.mod)){
       )
     )
   })
+
+  attr(filtered_data, "n_records_dropped") <- nrow(data) - nrow(filtered_data)
+  return(filtered_data)
+}
+
+
+#' Apply `null` mapping
+#' @inheritParams nm_data_filter
+#' @keywords internal
+nm_data_null_map <- function(.mod, data = nm_data(.mod)){
+  # Get data record
+  data_rec <- read_data_record(.mod)
+
+  # Extract & format NULL options (not used for filtering)
+  null_opts <- purrr::keep(data_rec$values, function(val){
+    inherits(val, "nmrec_option_value") && identical(val[["name"]], "null")
+  })
+  null_val <- purrr::map_chr(null_opts, function(ign_val){
+    unquote_filename(ign_val$value)
+  })
+
+  # Replace null values with `null_val`
+  null_valid <- !rlang::is_empty(null_val) && length(null_val) == 1
+  if (isTRUE(null_valid)) {
+    # Null data items consist of a single dot (.), consecutive commas or
+    # consecutive tab characters
+    is_nm_null <- function(val){
+      val == "." | grepl("^[,]{2,}$", val) | grepl("^[ \t]{2,}$", val)
+    }
+    data <- data %>% dplyr::mutate(
+      across(everything(), ~ ifelse(is_nm_null(.x), null_val, .x))
+    )
+    attr(data, "null_value") <- null_val
+  }
+  return(data)
+}
+
+#' Drop or skip records based on `$INPUT` record options
+#' @inheritParams nm_data_filter
+#' @keywords internal
+nm_data_drop_skip_records <- function(.mod, data = nm_data(.mod)){
+  # Get INPUT record
+  input_records <- get_records(.mod, "input")
+  if (length(input_records) != 1) {
+    rlang::abort("Expected a single $INPUT record.")
+  }
+
+  input_rec <- input_records[[1]]
+  input_rec$parse()
+
+  # Extract columns that should be dropped
+  drop_cols <- purrr::map_chr(input_rec$values, function(val){
+    if(inherits(val, c("nmrec_option_flag", "nmrec_option_value")) &&
+       (identical(val[["name"]], "DROP") | identical(val[["name"]], "SKIP"))){
+      ifelse(isTRUE(val$value), val[["name"]], val[["value"]])
+    }else{
+      return(NA)
+    }
+  })
+
+  cols_to_remove <- drop_cols[!is.na(drop_cols)]
+
+  if (length(cols_to_remove) > 0) {
+    data <- data %>% dplyr::select(-all_of(cols_to_remove))
+    attr(data, "dropped_cols") <- cols_to_remove
+  }
+
+  return(data)
+}
+
+
+#' Format input data as an `NM-TRAN` dataset
+#' @inheritParams nm_data_filter
+#' @keywords internal
+nm_data_as_nmtran <- function(.mod, data = nm_data(.mod)){
+
+  # Filter the data based on IGNORE and ACCEPT statements
+  filtered_data <- nm_data_filter(.mod, data = data)
+
+  # Map null values
+  null_mapped_data <- nm_data_null_map(.mod, data = filtered_data)
+
+  # DROP/SKIP records
+  final_data <- nm_data_drop_skip_records(.mod, data = null_mapped_data)
+
+  # Rename columns if data item labels were used
+  # TODO: confirm if renaming should happen before/after IGNORE options
+  col_names_ctl <- get_input_columns(.mod, from_data = FALSE)
+
+  # Filter out column names that were dropped
+  dropped_cols <- attributes(final_data)$dropped_cols
+  if(!is.null(dropped_cols)){
+    cols_rename <- col_names_ctl[!(col_names_ctl %in% dropped_cols)]
+  }
+
+  if(!all(cols_rename %in% names(final_data))){
+    rlang::abort(
+      c(
+        "At least one column specified in the control stream was not found in the data",
+        paste("Diff: ", setdiff(cols_rename, names(final_data)))
+      )
+    )
+  }
+
+  final_data <- final_data %>% setNames(names(cols_rename))
+
+  return(final_data)
 }
 
 #' Helper for checking if a specified record type is valid.
